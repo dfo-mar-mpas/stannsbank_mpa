5c65b93d376904d459ef93f76dbce96635fd4dfc
NickJeff13 <Nick.w.jeffery3@gmail.com>
2023-11-29 09:23:12 -0400

    updating script

--- code/crab_survey_extract_updated.R
+++ code/crab_survey_extract_updated.R
@@ -63,8 +63,7 @@ ggplot()+
 
 
 ###3. Do some data filtering and merging to just focus on the SAB inside and outside trawl stations
-#filter MPATOWS data by everything >45 degrees north to remove Gully stations 
-SABTOWS <- MPATOWS %>% filter(LATITUDE>45.1)
+SABTOWS <- MPATOWS %>% filter(LATITUDE>45.1) #filter MPATOWS data by everything >45 degrees north to remove Gully stations 
 SABTOWS$LONGITUDE <- SABTOWS$LONGITUDE*-1
 
 #transform into an sf object and find stations inside vs outside the MPA
@@ -83,46 +82,55 @@ ggplot()+
   theme_minimal()+
   theme(panel.background = element_rect(fill="lightblue"))
 
-SABTOWS2$COMM <- str_replace(SABTOWS2$COMM, pattern = "BASKET STARS; GORGONOCEPHALIDAE,ASTERONYCHIDAE", replacement = "BASKET STARS")
+SABTOWS2$COMM <- str_replace(SABTOWS2$COMM, pattern = "BASKET STARS; GORGONOCEPHALIDAE,ASTERONYCHIDAE", replacement = "BASKET STARS") #let's shorten this name so it plots nicer
 
 SABTOWS3 <- SABTOWS2  %>% filter(!is.na(COMM) & !grepl("SEAWEED, ALGAE ,KELP; THALLOPHYTA|APHIA|BARNACLES|SARSI|BUCCINIDAE|SLUGS|ISOPOD1|PEACH|RUSSIAN|UNIDENTIFIED|SEA MOUSE|EGGS",COMM) & !grepl("ASTERIAS|LAMPRETAE|LYCODES|CARIS|HORMATHIA|BOLOCERA|CALATHURA|DECAPODA|SCLEROCRANGON", SPEC)) 
 
 #Look at fish counts from the SAB tows
 # Create boxplots for fish catch by species
-ggplot(SABTOWS2, aes(x = Year, y = log(EST_NUM_CAUGHT))) +
+ggplot(SABTOWS3, aes(x = Year, y = log(EST_NUM_CAUGHT), colour=Inside)) +
   facet_wrap(vars(SPEC),nrow=12)+
   geom_boxplot() +
   labs(title = "# Caught by Species",
        x = "Species",
        y = "# Caught") +
   theme_minimal()+
-  theme(axis.text.x =  element_text(angle=90))
+  theme(axis.text.x =  element_text(angle=90),strip.text.x = element_text(size = 6))
 
-#####Nowlook at the morphology data (length and weight)
+#Let's do some diversity statistics
+#mutate data into a community matrix for a species accumulation curve
+spec.mat <-as.data.frame(SABTOWS3[,c("EST_NUM_CAUGHT","DEPTH","Inside","Year","STATION","SPEC","COMM")])
+spec.mat <-spec.mat[,-8]
+spec.mat2<-spec.mat[,]
+
+#####Now look at the morphology data (length and weight)
 # Group the data by species and calculate summary statistics
 summary_data <- fishmorph %>% na.omit(fishmorph) %>%
   group_by(COMM) %>%
   summarise(
     Mean_Length = mean(FISH_LENGTH, na.rm = TRUE),
+    Length_SD=sd(FISH_LENGTH, na.rm=TRUE),
     Median_Length = median(FISH_LENGTH, na.rm = TRUE),
     Mean_Weight = mean(MEASURED_WGT, na.rm = TRUE),
+    Weight_SD=sd(MEASURED_WGT, na.rm=TRUE),
     Median_Weight = median(MEASURED_WGT, na.rm = TRUE)
   )
 
 #Add a new column in fishmorph for year by extracting year from the BOARD DATE column
-fishmorph <- na.omit(fishmorph) %>% mutate(Year = format
-              (as.Date(BOARD_DATE, tryFormats = c("%Y-%m-%d", "%Y/%m/%d")),"%Y"))
+fishmorph2 <- na.omit(fishmorph) %>% mutate(Year = format
+              (as.Date(BOARD_DATE, tryFormats = c("%Y-%m-%d", "%Y/%m/%d")),"%Y")) 
+fishmorph2$COMM <-str_replace(fishmorph2$COMM, pattern="YELLOWTAIL FLOUNDER; LIMANDA", replacement = "YELLOWTAIL")
   
 # Create boxplots for fish lengths by species
-ggplot(fishmorph, aes(x = Year, y = FISH_LENGTH)) +
+ggplot(fishmorph2, aes(x = Year, y = FISH_LENGTH)) +
   facet_wrap(vars(COMM),nrow=8, scales="free_y")+
   geom_boxplot() +
   labs(title = "Fish Lengths by Species",
        x = "Species",
        y = "Length") +
   theme_minimal()+
-  theme(axis.text.x =  element_text(angle=90))+
-  stat_compare_means()
+  theme(axis.text.x =  element_text(angle=90),strip.text.x = element_text(size = 6))+
+  stat_compare_means(position = "jitter")
 
 ggsave(filename = "CrabSurvey_SAB_FishLengths.png",plot = last_plot(),device = "png",width = 22, height=14, units = "in",dpi = 500, bg = "white")
 # Create boxplots for fish weights by species
@@ -137,6 +145,7 @@ ggplot(fishmorph, aes(x = Year, y = MEASURED_WGT)) +
 
 
 ###Species Accumulation Curves for Inside vs Outside the MPA
+library(vegan)
 library(BiodiversityR)
 
 
