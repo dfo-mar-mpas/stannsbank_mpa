f086ed4a1c422d09844db8ef4d0c34c6eb089781
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2024-02-22 23:32:33 -0400

    Biodiversity & number inside/outside comps

--- code/crab_survey_analysis_RS.R
+++ code/crab_survey_analysis_RS.R
@@ -19,6 +19,14 @@
       capitalized_text <- paste(words, collapse = " ")  # Recombine the words into a single string
       return(capitalized_text)
     }
+    
+    reverselog_trans <- function(base = exp(1)) { #https://stackoverflow.com/questions/20924705/plot-negative-values-in-logarithmic-scale-with-ggplot-2
+      trans <- function(x) -log(x, base)
+      inv <- function(x) base^(-x)
+      trans_new(paste0("reverselog-", format(base)), trans, inv, 
+                log_breaks(base = base), 
+                domain = c(1e-100, Inf))
+    }
 
 #projections -----------
     latlong <- "+init=epsg:4326 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
@@ -73,7 +81,67 @@ colnames(stns)<-c("STATION", "Enhanced", "Inside")
                 rename_with(~str_remove(., '.x'))
 
 
-#Format the crab data
+    #standardize the data
+    ggplot()+
+      geom_boxplot(data=catchdat%>%distinct(TRIP_STATION,.keep_all=TRUE),aes(x=1,y=AREA_SWEPT))
+    
+    MedianAreaSwept <- catchdat%>%
+                       filter(!is.na(AREA_SWEPT))%>%
+                       distinct(TRIP_STATION,.keep_all=TRUE)%>%
+                       pull(AREA_SWEPT)%>%
+                       median()
+    
+    #standardize by the median area swept
+    catchdat_stand <- catchdat%>%
+                      filter(!is.na(AREA_SWEPT))%>%
+                      mutate(number = EST_NUM_CAUGHT * MedianAreaSwept/AREA_SWEPT,
+                             weight = EST_DISCARD_WT * MedianAreaSwept/AREA_SWEPT,
+                             COMM = ifelse(COMM == "",NA,COMM),
+                             SPEC = ifelse(SPEC =="",NA,SPEC),
+                             species_filter = ifelse(is.na(SPEC),COMM,SPEC),
+                             station = as.integer(STATION))%>% #get rid of unidentified species
+                      filter(!is.na(species_filter))%>%
+                      left_join(.,stns%>%rename(station=STATION))%>%
+                      mutate(location = ifelse(Inside,"Inside","Outside"),
+                             type = ifelse(Enhanced,"Enhanced","Standard"),
+                             year=as.numeric(format(as.Date(BOARD_DATE, tryFormats = c("%Y-%m-%d", "%Y/%m/%d")),"%Y")))
+ 
+    
+    catchdat_sf <- catchdat_stand%>%
+                   distinct(TRIP_STATION,.keep_all=TRUE)%>%
+                   mutate(LONGITUDE = LONGITUDE *-1)%>%
+                   st_as_sf(coords=c("LONGITUDE","LATITUDE"),crs=latlong)
+                  
+    ggplot()+geom_sf(data=mpas)+geom_sf(data=catchdat_sf) #all up by the MPA
+    
+#Save output for taxonomic cleaning -- run Crab_survey_taxonomy.R to get the full cleaned list
+    write.csv(catchdat_stand%>%
+                select(COMM,SPEC,species_filter)%>%
+                distinct(species_filter,.keep_all=TRUE),"output/taxonomic_raw.csv",row.names=FALSE)
+    
+#load the cleaned taxonomy
+    PhyloNames <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+    
+    crab_tax_clean <- read.csv("output/crab_taxa_clean.csv")%>%
+                      select(all_of(PhyloNames),aphiaID,common,species_filter)%>%
+                      rowwise()%>%
+                      mutate(taxcount = sum(c(is.na(Kingdom),is.na(Phylum),is.na(Class),
+                                            is.na(Order),is.na(Family),is.na(Genus),is.na(Species))))%>%
+                      data.frame()
+
+    #get the minimum identification    
+    crab_tax_clean$minID <- crab_tax_clean$Species
+                      
+    for(i in 1:nrow(crab_tax_clean)){
+      
+      if(crab_tax_clean[i,"taxcount"]!=0){crab_tax_clean[i,"minID"] <- crab_tax_clean[i,length(PhyloNames)-crab_tax_clean[i,"taxcount"]]}
+      
+    }
+    
+#link in the taxonomy data
+    catchdat_stand <- catchdat_stand%>%left_join(.,crab_tax_clean)
+
+#Format the crab data ---------
     load("data/CrabSurvey/OldData/MPATOWS.RDATA") #load this one so we can make a map of the Gully and SAB stations, this is only the enhanced stations
     
     MPATOWS <- MPATOWS%>%subset(., select = which(!duplicated(names(.))))
@@ -167,27 +235,13 @@ colnames(stns)<-c("STATION", "Enhanced", "Inside")
                data.frame()
     
     
-    plot_sp <- plotftr%>%
-               distinct(species2,.keep_all=TRUE)%>%
-               filter(inboth & enoughyears)%>%
-               pull(species2)
-    
     plot_df <- plotftr%>%filter(inboth & enoughyears & prepost)
-               
-                  
-                  
-    
-    ggplot()+
-      geom_vline(xintercept = 2017,lty=2)+
-      geom_line(data=bigfish_df%>%filter(species2 %in% plot_sp),aes(x=year,y=stand_length,col=location,group=station))+
-      geom_point(data=bigfish_df%>%filter(species2 %in% plot_sp),aes(x=year,y=stand_length,col=location,group=station),size=2)+
-      theme_bw()+
-      facet_wrap(~species,nrow=3)
+     
     
     #Median size tracked over time with smooth
     p_med_a <- ggplot()+
       geom_vline(xintercept = 2017,lty=2)+
-      geom_line(data=plot_df,aes(x=year,y=med,col=location,group=station),lwd=0.5,alpha=0.5)+
+      geom_line(data=plot_df,aes(x=year,y=med,col=location,group=station),lty=2,lwd=0.25,alpha=0.5)+
       geom_point(data=plot_df,aes(x=year,y=med,col=location,group=station),size=2,alpha=0.5)+
       theme_bw()+
       stat_smooth(data=plot_df,aes(x=year,y=med,col=location),lwd=2,se=FALSE)+
@@ -208,8 +262,8 @@ colnames(stns)<-c("STATION", "Enhanced", "Inside")
     #90th percentile size tracked over time with smooth
     p_bigfish_a <- ggplot()+
       geom_vline(xintercept = 2017,lty=2)+
-      geom_line(data=plot_df,aes(x=year,y=bigfish,col=location,group=station),lwd=0.5,alpha=0.5)+
-      geom_point(data=plot_df,aes(x=year,y=bigfish,col=location,group=station),size=2,alpha=0.5)+
+      geom_line(data=plot_df,aes(x=year,y=bigfish,col=location,group=station),lty=2,lwd=0.25,alpha=0.5)+
+      geom_point(data=plot_df,aes(x=year,y=bigfish,col=location,group=station),size=2,alpha=0.55)+
       theme_bw()+
       stat_smooth(data=plot_df,aes(x=year,y=bigfish,col=location),lwd=2,se=FALSE)+
       facet_wrap(~species2,nrow=3,scales="free_y")+
@@ -225,6 +279,109 @@ colnames(stns)<-c("STATION", "Enhanced", "Inside")
       
     ggsave("output/CrabSurvey/LargeFish_inside-outside_wSmooth.png",p_bigfish_a)
     ggsave("output/CrabSurvey/LargeFish_inside-outside.png",p_bigfish_b)    
+    
+    
+##### Catch changes within stations --------------
+    
+    ##biodiversity per set
+    biodiv_df <- catchdat_stand%>%
+                 filter(type=="Enhanced")%>%
+                 group_by(TRIP_STATION)%>%
+                 summarise(richness=length(unique(species_filter)))%>%
+                 ungroup()%>%
+                 left_join(.,catchdat_stand%>%
+                             distinct(TRIP_STATION,.keep_all=TRUE)%>%
+                             select(TRIP_STATION,location,type,station,year))%>%
+                 select(location,type,TRIP_STATION,station,richness,year)%>%
+                 group_by(station)%>%
+                 mutate(richness_stand = richness/max(richness))%>%
+                 ungroup()%>%
+                 data.frame()
+                
+    p_richness_a <- ggplot()+
+      geom_vline(xintercept = 2017,lty=2)+
+      geom_line(data=biodiv_df,aes(x=year,y=richness,group=station,col=location),lty=2,lwd=0.25)+
+      geom_point(data=biodiv_df,aes(x=year,y=richness,group=station,col=location),size=2,alpha=0.5)+
+      theme_bw()+
+      labs(col="",x="Year",y="Richness per station")+
+      theme(legend.position = "bottom");p_richness_a
+    
+    p_richness_b <- ggplot()+
+      geom_vline(xintercept = 2017,lty=2)+
+      geom_line(data=biodiv_df,aes(x=year,y=richness,group=station,col=location),lty=2,lwd=0.25,alpha=0.5)+
+      geom_point(data=biodiv_df,aes(x=year,y=richness,group=station,col=location),size=2,alpha=0.5)+
+      stat_smooth(data=biodiv_df,aes(x=year,y=richness,col=location),lwd=2)+
+      theme_bw()+
+      labs(col="",x="Year",y="Richness per station")+
+      theme(legend.position = "bottom");p_richness_b
+    
+    ggsave("output/CrabSurvey/Biodiversity_inside-outside.png",p_richness_a)
+    ggsave("output/CrabSurvey/Biodiversity_inside-outside_wSmooth.png",p_richness_b)
+    
+    ##catch number per set
+    
+    freq_sp <- catchdat_stand%>%
+               filter(minID != "Actiniaria")%>% #sea anemones are captures but not really a species ae can tell a story around 
+               group_by(minID)%>%
+               summarise(count=n())%>%
+               ungroup()%>%
+               arrange(-count)%>%
+               slice(1:10)%>%
+               pull(minID)%>%
+               c(.,"Anarhichas lupus","Merluccius bilinearis")
+    
+    # num_df <- catchdat_stand%>%
+    #           filter(minID %in% freq_sp)%>%
+    #           group_by(minID,year,location)%>%
+    #           summarise(number_ave = mean(number,na.rm=T))%>%
+    #           ungroup()%>%
+    #           spread(location,number_ave)%>%
+    #           group_by(minID,year)%>%
+    #           summarise(diff=Inside-Outside)%>%
+    #           ungroup()%>%
+    #           data.frame()%>%
+    #           filter(!is.na(diff))
+    # 
+    # num_comp <- ggplot(data=num_df,aes(x=year,y=diff,group=species_filter))+
+    #             #geom_line()+
+    #             stat_smooth(data=num_df,aes(x=year,y=diff))+
+    #             geom_vline(xintercept = 2017,lty=2)+
+    #             #scale_y_continuous(trans=weird)+
+    #             theme_bw();num_comp #looks like shit!!
+    #                      
+              
+    num_df <- catchdat_stand%>%
+              filter(minID %in% freq_sp,
+                     station %in% stns$STATION)%>%
+              mutate(station=as.integer(STATION))%>%
+              group_by(location,station,minID)%>%
+              mutate(num_stand = number/max(number,na.rm=T))%>%
+              ungroup()%>%
+              data.frame()
+      
+      
+    p_number_a <- ggplot()+
+                  geom_vline(xintercept = 2017,lty=2)+
+                  geom_line(data=num_df,aes(x=year,y=number,col=location,group=station),lty=2,lwd=0.25,alpha=0.5)+
+                  geom_point(data=num_df,aes(x=year,y=number,col=location,group=station),size=2,alpha=0.55)+
+                  theme_bw()+
+                  stat_smooth(data=num_df,aes(x=year,y=number,col=location),lwd=2,se=FALSE)+
+                  scale_y_log10()+
+                  facet_wrap(~minID,nrow=3,scales="free_y")+
+                  labs(col="",x="Year",y="Count per set");p_number_a
+                
+    p_number_b <- ggplot()+
+                  geom_vline(xintercept = 2017,lty=2)+
+                  geom_line(data=num_df,aes(x=year,y=num_stand,col=location,group=station),lty=2,lwd=0.25,alpha=0.5)+
+                  geom_point(data=num_df,aes(x=year,y=num_stand,col=location,group=station),size=2,alpha=0.55)+
+                  theme_bw()+
+                  stat_smooth(data=num_df,aes(x=year,y=num_stand,col=location),lwd=2)+
+                  facet_wrap(~minID,nrow=3,scales="free_y")+
+                  labs(col="",x="Year",y=expression(paste("Count per set / Max count per set")));p_number_b
+                
+    ggsave("output/CrabSurvey/CatchNumber_inside-outside.png",p_number_a)
+    ggsave("output/CrabSurvey/CatchNumber_inside-outside_standardized.png",p_number_b)
+    
     
 #### DIET ANALYSIS --------------
     
