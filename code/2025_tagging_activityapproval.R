66a86b2dd0503d90163326cb2589438500408b21
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2025-12-16 14:06:45 -0400

    update map with bathymetry

--- code/2025_tagging_activityapproval.R
+++ code/2025_tagging_activityapproval.R
@@ -3,11 +3,14 @@ library(tidyverse)
 library(sf)
 library(rnaturalearth)
 library(terra)
+library(tidyterra)
 library(stars)
 library(ggspatial)
 library(ggnewscale)
 library(viridis)
 library(patchwork)
+library(dbscan)
+library(cmocean)
 
 s2_as_sf = FALSE
 
@@ -32,10 +35,65 @@ sab  <- sab_zones%>%
   st_transform(latlong)%>%
   st_as_sf()
 
+#Load the bigh resolution bathymetry 
+sab_highres <- rast("data/Bathymetry/SAB_highres/fullBathy_projected_latlong.tif")%>%
+               crop(.,st_bbox(sab))
+
+bathy_proj <- st_crs(sab_highres)
+
 #load the banks shapfile
 sab_banks <- read_sf("data/Shapefiles/sab_banks_40m.shp")%>%
              st_transform(latlong)%>%
              st_polygonize()
+
+curdo <- sab_banks%>%
+         filter(name=="Curdo Bank")%>%
+         st_simplify()%>%
+         st_make_valid()
+
+scatarie <- sab_banks%>%
+  filter(name=="Scatarie Bank")%>%
+  st_simplify()%>%
+  st_make_valid()
+
+#bank-based bathymetry
+curdo_bathy <- sab_highres%>%
+               crop(.,st_bbox(curdo))%>%
+               terra::mask(.,curdo)
+
+scatarie_bathy <- sab_highres%>%
+                  crop(.,st_bbox(scatarie))%>%
+                  terra::mask(.,scatarie)
+
+#trim to 35m for the plot
+curdo_35m_contour <- as.contour(curdo_bathy, levels = -35)%>%
+                      st_as_sf()%>%
+                      st_set_crs(latlong)%>%
+                      st_cast("POLYGON")%>%
+                      mutate(name="Curdo Bank")
+
+scatarie_35m_contour <- as.contour(scatarie_bathy, levels = -35)%>%
+                        st_as_sf()%>%
+                        st_set_crs(latlong)%>%
+                        st_cast("POLYGON")%>%
+                        mutate(name="Scatarie Bank")
+
+curdo_bathy_35 <- curdo_bathy%>%
+                  terra::mask(.,curdo_35m_contour)
+
+scatarie_bathy_35 <- scatarie_bathy%>%
+                    terra::mask(.,scatarie_35m_contour)
+
+ggplot()+
+  geom_spatraster(data=curdo_bathy_35)+
+  geom_sf(data=curdo,fill=NA)+
+  scale_fill_viridis()
+
+ggplot()+
+  geom_spatraster(data=scatarie_bathy_35)+
+  geom_sf(data=scatarie,fill=NA)+
+  scale_fill_viridis()
+
 
 #load the reciever locations
 reciever_locs_old <- read.csv("data/Acoustic/OTN_redesign_coords.csv")%>%
@@ -104,6 +162,23 @@ tag_2023_25 <- read.csv("data/Acoustic/SABMPA_tagging_basic_metadata_all_QCApril
 tag_df <- rbind(tag_2022,tag_2023_25)
 
 
+#create a tag table (this will summarize the points)
+coords <- tag_df%>%
+          st_transform(utm)%>%
+          st_coordinates()
+          
+clusters <- dbscan(coords, eps = 1, minPts = 1)
+
+tag_df_summary <- tag_df %>%
+  mutate(cluster_id = clusters$cluster) %>%
+  group_by(cluster_id, year) %>%
+  summarise(
+    n_points = n(),
+    geometry = st_centroid(st_union(geometry)),
+    .groups = "drop"
+  ) %>%
+  st_as_sf()
+
 #save coordinates for the activity approval applcation - to simplify the included table, the 2022 tag locations will be presented as centriods of the three clusters
 mod_sets <- tag_2022%>%
             mutate(cluster = case_when(lon> -59.26 ~ 1, #rougly 4 clusters
@@ -144,52 +219,73 @@ scatarie_bound <- sab_banks%>%
 curdo_plot <- ggplot() +
   geom_sf(data=shelfbreak,fill=NA,lwd=0.2)+
   geom_sf(data=basemap)+
-  geom_sf(data=sab_banks,fill="cornflowerblue")+
-  geom_sf(data=reciever_locs,aes(fill=type),pch=21,size=2)+
-  geom_sf(data=tag_df,aes(fill=type),size=1.25*1.2,pch=21)+
+  geom_spatraster(data=curdo_bathy_35)+
+  geom_sf(data=reciever_locs,fill="white",pch=21,size=2)+
+  geom_sf(data=tag_df_summary,shape=21,col="coral")+
   coord_sf(expand=0,xlim=curdo_bound[c(1,3)],ylim=curdo_bound[c(2,4)])+
+  scale_fill_cmocean(name = "deep", direction = -1, na.value = NA)+
   theme_bw()+
   theme(axis.text=element_blank(),
-        legend.position = "none")+
-  scale_fill_manual(values=c("white","grey"))+
-  annotation_scale(location="br")+
-  labs(title="Curdo Bank")
+        legend.position = "inside",
+        legend.position.inside = c(0.9,0.8),
+        legend.background = element_rect(color="black"),
+        legend.key.height = unit(0.3/2, "cm"),
+        legend.key.width = unit(0.25/2, "cm"),
+        legend.text = element_text(size = 5*0.8),
+        legend.title = element_text(size = 5*0.7))+
+  annotation_scale(location="bl")+
+  labs(title="Curdo Bank",
+       fill="Depth (m)")
 
 scatarie_plot <- ggplot() +
-  geom_sf(data=shelfbreak,fill=NA,lwd=0.2)+
-  geom_sf(data=basemap)+
-  geom_sf(data=sab_banks,fill="cornflowerblue")+
-  geom_sf(data=reciever_locs,aes(fill=type),pch=21,size=2)+
-  geom_sf(data=tag_df,aes(fill=type),size=1.25*1.2,pch=21)+
-  coord_sf(expand=0,xlim=scatarie_bound[c(1,3)],ylim=scatarie_bound[c(2,4)])+
-  theme_bw()+
-  theme(axis.text=element_blank(),
-        legend.position = "none")+
-  scale_fill_manual(values=c("white","grey"))+
-  annotation_scale(location="br")+
-  labs(title="Scatarie Bank")
+                geom_sf(data=shelfbreak,fill=NA,lwd=0.2)+
+                geom_sf(data=basemap)+
+                #geom_sf(data=sab_banks,fill=NA)+
+                geom_spatraster(data=scatarie_bathy_35)+
+                geom_sf(data=reciever_locs,fill="white",pch=21,size=2)+
+                #geom_sf(data=tag_df,size=1.25*1.2,col="black")+
+                geom_sf(data=tag_df_summary,col="black")+
+                coord_sf(expand=0,xlim=scatarie_bound[c(1,3)],ylim=scatarie_bound[c(2,4)])+
+                scale_fill_cmocean(name = "deep", direction = -1, na.value = NA)+
+                theme_bw()+
+                theme(axis.text=element_blank(),
+                      legend.position = "none")+
+                annotation_scale(location="bl")+
+                labs(title="Scatarie Bank")
 
 plot_boundaries <- c(c(-59.7,-58.3,45.7,46.5))
 
 #make plot
+
+# Calculate hillshade from your DEM
+# hill <- terrain(sab_highres, v = "slope", neighbors = 8)
+# shade <- terrain(sab_highres, v = "aspect", neighbors = 8)
+# 
+# # Create hillshade (0-1 scale)
+# hillshade <- shade(hill, shade)
+# 
+# # Convert to data frame for plotting
+# hillshade_df <- as.data.frame(hillshade, xy = TRUE)
+
+
 sab_plot <- ggplot()+
-  geom_sf(data=shelfbreak,fill=NA,lwd=0.2)+
   geom_sf(data=basemap)+
+  geom_spatraster(data=sab_highres)+
   geom_sf(data=sab_zones,fill=NA,linewidth=1.1,col="grey60")+
-  geom_sf(data=sab_banks,fill="cornflowerblue")+
+  geom_sf(data=sab_banks,fill="grey",alpha=0.5)+
   geom_sf(data=curdo_bound%>%st_as_sfc(),fill=NA)+
   geom_sf(data=scatarie_bound%>%st_as_sfc(),fill=NA)+
-  geom_sf(data=reciever_locs,aes(fill=type),pch=21)+
-  geom_sf(data=tag_df,aes(fill=type),size=1.25,pch=21)+
-  #coord_sf(expand=0)+
+  geom_sf(data=reciever_locs,fill="white",pch=21)+
+  geom_sf(data=tag_df,size=0.5,pch=3)+
+  geom_sf(data=tag_df_summary,size=2,col="grey60",pch=21)+
+  scale_fill_cmocean(name = "deep", direction = -1, na.value = NA)+
   coord_sf(xlim=plot_boundaries[c(1,2)],ylim=plot_boundaries[c(3,4)],expand=0)+
-  scale_fill_manual(values=c("white","grey"))+
-  labs(fill="")+
+  labs(fill="Depth (m)")+
   theme_bw()+
-  theme(panel.grid = element_blank(),
+  theme(
         legend.position = "inside",
-        legend.position.inside = c(0.8,0.15),
-        legend.title = element_blank(),
+        legend.position.inside = c(0.9,0.22),
+        legend.background = element_rect(color="black")
         )+
   annotation_scale(location="bl")
 
